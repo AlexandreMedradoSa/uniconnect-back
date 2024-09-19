7cec7c0a480e14fd3afa38f89b69a8b1134e8206
unknown <alexandremedradosa@gmail.com>
2024-09-19 16:51:16 -0300

    Rotas Para Deletar: Cursos e Interesses

--- src/index.js
+++ src/index.js
@@ -20,10 +20,12 @@ app.put('/api/profile', verifyToken, authController.updateProfile);
 app.get('/api/interesses', verifyToken, authController.getInteresses);
 app.post('/api/interesses', verifyToken, authController.addInteresse);
 app.put('/api/interesses/:id', verifyToken, authController.updateInteresse);
+app.delete('/api/interesses/:id', verifyToken, authController.deleteInteresse);
 
 app.get('/api/cursos', verifyToken, authController.getCursos);
 app.post('/api/cursos', verifyToken, authController.addCurso);
 app.put('/api/cursos/:id', verifyToken, authController.updateCurso);
+app.delete('/api/cursos/:id', verifyToken, authController.deleteCurso);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
